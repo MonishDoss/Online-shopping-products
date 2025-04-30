7f9f602eb47b87f7060f11b6df13d982d785f3ea
MonishDoss <monishdoss2005@gmail.com>
2025-05-01 00:26:54 +0530

    Creacting Spring-Boot project using Spring start.spring.io

---
+++ backend(Spring Boot)/Online-Shopping-Products/src/main/java/com/KCG/Online_Shopping_Products/OnlineShoppingProductsApplication.java
@@ -0,0 +1,13 @@
+package com.KCG.Online_Shopping_Products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShoppingProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShoppingProductsApplication.class, args);
+	}
+
+}
---
+++ backend(Spring Boot)/Online-Shopping-Products/src/test/java/com/KCG/Online_Shopping_Products/OnlineShoppingProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KCG.Online_Shopping_Products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShoppingProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
